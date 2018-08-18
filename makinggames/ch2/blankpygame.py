93854b00b683259f5932ccfcfcc077d73e5ef1e8
vitalwarley <wvb@ic.ufal.br>
2018-08-18 16:35:36 -0300

    Updated .gitignore.

--- makinggames/ch2/blankpygame.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created by lativ on 13/08/18 at 21:16
-"""
--- makinggames/ch2/drawing.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created by lativ on 14/08/18 at 16:23
-"""
