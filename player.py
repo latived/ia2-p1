ea785f91071da3e2247571378728326be63ed2f2
vitalwarley <wvb@ic.ufal.br>
2018-08-19 07:21:17 -0300

    Player class.

--- player.py
+++ player.py
@@ -3,3 +3,13 @@
 """
 Created by lativ on 19/08/18 at 07:02
 """
+
+
+class Player():
+
+    def __init__(self, name, location, action_points, mov_points, atk_types):
+        self.name = name
+        self.position = location
+        self.action_points = action_points
+        self.movement_points = mov_points
+        self.atk_types = atk_types
